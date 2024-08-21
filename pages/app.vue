37821ab760cb375e34a1d4e51d49113599093c46
emanuel <emanuel@modularia.com.br>
2024-08-21 02:07:30 +0000

    proje init

--- pages/app.vue
+++ pages/app.vue
@@ -1,19 +1,27 @@
 
 <template>
   <div class="bg-gray-100 h-screen">
-  <div class="bg-gray-100 flex flex-col items-center">
-    <h1 class="text-3xl font-bold mt-10">Tarefas</h1>
-  </div>
-  <div class="bg-gray-100 h-screen  w-screen flex  items-center">
+    <div class="bg-gray-100 flex flex-col items-center">
+      <h1 class="text-3xl font-bold mt-10">Tarefas</h1>
+    </div>
+    
+    
+  
+    <div class="bg-gray-100 h-screen  w-screen flex gap-y-4  flex-col items-center">
+      <button class="w-3/4 flex content-center flex-row  items-center gap-3 p-4 rounded-md bg-white shadow-sm">
+        <span class="text-2xl font-bold center">+ </span>
+      </button>
 
-   <div v-for="task in Tasks" :key="task.title" class="flex flex-col items-center">
-    <SimpleTaskCard :title="task.title" :subtitle="task.subtitle" />
-   </div>
+
+      <div v-for="task in Tasks" :key="task.title" class="w-3/4 ">
+        <SimpleTaskCard :title="task.title" :subtitle="task.subtitle" />
+      </div>
+    </div>
   </div>
-  </div>
+
 </template>
 
-<script lang="ts">
+<script setup lang="ts">
 import SimpleTaskCard from '~/components/SimpleTaskCard.vue';
 
 const Tasks = [
