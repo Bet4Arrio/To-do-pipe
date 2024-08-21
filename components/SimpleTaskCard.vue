405210643b7aac98c3710b05c32a39a0bf566495
emanuel <emanuel@modularia.com.br>
2024-08-21 01:52:00 +0000

    project init

---
+++ components/SimpleTaskCard.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="flex items-center gap-3 p-4 rounded-md bg-white shadow-sm">
+    <input type="checkbox" class="w-4 h-4 rounded-md" />
+    <div>
+      <h3 class="text-lg font-medium">{{ title }}</h3>
+      <p class="text-gray-500">{{ subtitle }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    subtitle: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
---
+++ pages/app.vue
@@ -0,0 +1,35 @@
+
+<template>
+  <div class="bg-gray-100 h-screen">
+  <div class="bg-gray-100 flex flex-col items-center">
+    <h1 class="text-3xl font-bold mt-10">Tarefas</h1>
+  </div>
+  <div class="bg-gray-100 h-screen  w-screen flex  items-center">
+
+   <div v-for="task in Tasks" :key="task.title" class="flex flex-col items-center">
+    <SimpleTaskCard :title="task.title" :subtitle="task.subtitle" />
+   </div>
+  </div>
+  </div>
+</template>
+
+<script lang="ts">
+import SimpleTaskCard from '~/components/SimpleTaskCard.vue';
+
+const Tasks = [
+  {
+    title: "task 1",
+    subtitle: "task 2",
+  },
+  {
+    title: "task 1",
+    subtitle: "task 2",
+  },
+  {
+    title: "task 1",
+    subtitle: "task 2",
+  },
+]
+
+
+</script>
